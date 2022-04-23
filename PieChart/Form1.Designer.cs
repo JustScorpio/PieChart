ea508ba4d38358aa004c16f16615b0e2a7ab203e
Kosarev <skorpiello@gmail.com>
2022-04-23 14:46:03 +0500

    Добавьте файлы проекта.

---
+++ PieChart/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace PieChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFirstNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSecondNum = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(348, 308);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "5 случайных чисел";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(263, 326);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(97, 23);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Сгенерировать";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "-";
+            // 
+            // textBoxFirstNum
+            // 
+            this.textBoxFirstNum.Location = new System.Drawing.Point(126, 326);
+            this.textBoxFirstNum.Name = "textBoxFirstNum";
+            this.textBoxFirstNum.Size = new System.Drawing.Size(37, 20);
+            this.textBoxFirstNum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Диапазон значений:";
+            // 
+            // textBoxSecondNum
+            // 
+            this.textBoxSecondNum.Location = new System.Drawing.Point(185, 326);
+            this.textBoxSecondNum.Name = "textBoxSecondNum";
+            this.textBoxSecondNum.Size = new System.Drawing.Size(37, 20);
+            this.textBoxSecondNum.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 356);
+            this.Controls.Add(this.textBoxSecondNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFirstNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFirstNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSecondNum;
+    }
+}
+
---
+++ PieChart/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PieChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+            textBoxFirstNum.Text = "1";
+            textBoxSecondNum.Text = "20";
+
+            Random rd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int randomNum = rd.Next(1, 20);
+                chart1.Series[0].Points.AddY(randomNum);
+                chart1.Series[0].Points[i].LegendText = (i + 1).ToString();
+                chart1.Series[0].Points[i].Label = randomNum.ToString();
+            }
+
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            var firstNum = 0;
+            var secondNum = 0;
+
+            try
+            {
+                firstNum = byte.Parse(textBoxFirstNum.Text);
+                secondNum = byte.Parse(textBoxSecondNum.Text);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Введите в поля диапазона положительные целые числа",
+                    "Нет, не так",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+
+                textBoxFirstNum.Text = string.Empty;
+                textBoxSecondNum.Text = string.Empty;
+                return;
+            }
+
+            Random rd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int randomNum = rd.Next(firstNum, secondNum);
+                chart1.Series[0].Points[i].YValues[0] = randomNum;
+                chart1.Series[0].Points[i].LegendText = (i + 1).ToString();
+                chart1.Series[0].Points[i].Label = randomNum.ToString();
+            }
+        }
+    }
+}
